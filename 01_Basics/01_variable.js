2ee1ca226a59dc43cc3d93ca838d066807726c2a
lonecodebreaker <75729507+lonecodebreaker@users.noreply.github.com>
2024-11-20 14:48:40 +0000

    variable study

---
+++ 01_Basics/01_variable.js
@@ -0,0 +1,6 @@
+const accountId = 123;
+let accountEmail = "shubham@gmail.com"
+var accoutPassword  = "000"
+accountCity = "pune"
+
+console.table([accountId,accountEmail,accoutPassword,accountCity])
