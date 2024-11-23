9a2bbd50ba40446b4d774a3436f75bbd19b141f3
lonecodebreaker <75729507+lonecodebreaker@users.noreply.github.com>
2024-11-23 16:02:01 +0000

    datatype learning

---
+++ 01_Basics/02_datatype.js
@@ -0,0 +1,28 @@
+ "use strict" //treat all JS code as newer version
+ 
+  //alert(3+3) gives error as we are using node.js , not browser
+
+  console.log(3+3); console.log("shubham")
+  // if we remove ; it will give error for same line 
+  // but for diffrent lines there is no need to put semicolon
+
+  let name = "shubham"
+  let age = 18
+  let isLoggedIn = false
+
+  console.log(typeof "shubham")
+  console.log(typeof 18)
+  console.log(typeof null)
+  console.log(typeof undefined)
+
+
+  // Primitive DATATYPES
+  // number => 2 to power 53
+  // bigint
+  // string => true/false
+  // null => standalone value (datatype)
+  // undefined =>
+  // symbol => datatype mostly used in react for uniqueness
+
+  //NON-Primitive  
+  // object
