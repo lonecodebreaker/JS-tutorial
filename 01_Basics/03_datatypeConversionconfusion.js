2c207713044836e312ead11606f3e9ae82e5df20
lonecodebreaker <75729507+lonecodebreaker@users.noreply.github.com>
2024-11-23 16:19:22 +0000

    datatype operations

--- 01_Basics/03_datatypeConversionconfusion.js
+++ 01_Basics/03_datatypeConversionconfusion.js
@@ -1,1 +1,12 @@
-let score = "33abc"
+let score = undefined
+//console.log(typeof score);
+let valueInNumber = Number(score);
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+let isLoggedIn  = "shubham";
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn)
+
+
+//*******************>OPERATIONS<******************
