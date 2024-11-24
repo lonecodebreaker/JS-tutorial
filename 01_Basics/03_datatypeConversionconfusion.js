ffb4061b41989368adfd2a12d77f19734b2ce9bb
lonecodebreaker <75729507+lonecodebreaker@users.noreply.github.com>
2024-11-24 09:30:44 +0000

    datatype complete

--- 01_Basics/03_datatypeConversionconfusion.js
+++ 01_Basics/03_datatypeConversionconfusion.js
@@ -10,3 +10,40 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 
 
 //*******************>OPERATIONS<******************
+
+let value = 3
+let negValue = -value
+// console.log(negValue );
+
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/3);
+// console.log(2%3);
+
+
+let str1 = "hello"
+let str2 = " shubham"
+
+let str3 = str1 + str2
+//console.log(str3); 
+
+//console.log("1"+ 2);
+//console.log(1 +"2");
+
+//diffrence in addition in below lines is because of rules in ECMA script
+//console.log("1"+2 +2);
+//console.log(1 + 2 + "3");
+
+//console.log(+true);
+//console.log(true+);
+
+let num1 , num2 , num3
+
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+++gameCounter
+console.log(gameCounter)
+
+// link to study postfix and prefix deeply
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
