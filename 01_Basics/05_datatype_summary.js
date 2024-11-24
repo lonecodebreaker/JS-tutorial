7086237ff9d346c921a393cccdb36e85b95e2c14
lonecodebreaker <75729507+lonecodebreaker@users.noreply.github.com>
2024-11-24 10:35:31 +0000

    datatype full eplained

---
+++ 01_Basics/05_datatype_summary.js
@@ -0,0 +1,23 @@
+// primitive
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+
+// Non-primitive
+// Array, Objects , Functions 
+
+const score  = 100;
+const scoreValue = 100.3
+const isLoggedIn  = false
+const outsideTemprature = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id === anotherId);
+
+//must read for interview prepartion (typeof) results for primitive and non primitive datatypes
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/typeof
+
+
+const bigNumber = 12345678909876543245667786567877n
+console.log(typeof bigNumber);
