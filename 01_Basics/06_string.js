bd6936b924eccb06b8a42e20147a67bda807bc5e
lonecodebreaker <75729507+lonecodebreaker@users.noreply.github.com>
2024-12-08 12:19:12 +0000

    string_number_math

--- 01_Basics/06_string.js
+++ 01_Basics/06_string.js
@@ -5,4 +5,24 @@
 
 console.log(`Hello my name is ${name} and my repo Count is  ${repoCount}`);
 
+ const gameName = new String('Shubham')
+ console.log(gameName[0]);
+ console.log(gameName.__proto__);
+ console.log(gameName.length);
+ console.log(gameName.toUpperCase());
+ console.log(gameName.charAt(1));
+
+ const newString  = gameName.substring(0,4)
+ console.log(newString);
+
+ //const anotherString = gameName.slice(-6,4);
+ console.log(gameName.slice(-6,4));
+
+ const newStringOne  = "    Shubham  "
+ console.log(newStringOne);
+ console.log(newStringOne.trim());
+ 
+ 
+ 
+ 
  
---
+++ 01_Basics/07_nums_and_maths.js
@@ -0,0 +1,32 @@
+const score = 100
+//console.log(score);
+
+const balance  = new Number(100)
+//console.log(balance);
+
+//console.log(balance.toString().length);
+//console.log(balance.toFixed(1));
+
+const hundreds  = 100000
+
+//console.log(hundreds.toLocaleString('en-In'))
+
+
+// +++++++++++++++++++++++++ Maths +++++++++++++++++++++++++++++++++
+
+
+//console.log(Math);
+//console.log(Math.abs(-4))
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.6));
+// console.log(Math.floor(4.6));
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random()*(max-min+1)) + min);
+
+
+
+
+
