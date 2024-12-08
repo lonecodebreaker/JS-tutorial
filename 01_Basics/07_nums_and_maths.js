704cc75e8fb744653313dd7fd08024efbd72e34c
lonecodebreaker <75729507+lonecodebreaker@users.noreply.github.com>
2024-12-08 16:21:22 +0000

    date_time

--- 01_Basics/07_nums_and_maths.js
+++ 01_Basics/07_nums_and_maths.js
@@ -22,11 +22,7 @@ const hundreds  = 100000
 // console.log(Math.floor(4.6));
 
 const min = 10;
-const max = 20;
+const max = 30;
 
 console.log(Math.floor(Math.random()*(max-min+1)) + min);
-
-
-
-
-
+ 
---
+++ 01_Basics/08_date_and_time.js
@@ -0,0 +1,10 @@
+let myDate  = new Date()
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleString());
+
+console.log(typeof myDate);
+
+let myCreatedDate = new Date(2024,11,8)
+console.log(myCreatedDate.toDateString());
+
+
