94fd91a585152b65a38baa3942eff1bf66b1e8c7
lonecodebreaker <75729507+lonecodebreaker@users.noreply.github.com>
2024-12-10 11:31:58 +0000

    date_and_time_complete

--- 01_Basics/08_date_and_time.js
+++ 01_Basics/08_date_and_time.js
@@ -1,10 +1,28 @@
 let myDate  = new Date()
+console.log(myDate);
 console.log(myDate.toDateString());
 console.log(myDate.toLocaleString());
 
+console.log(myDate.getDate());
+console.log(myDate.getFullYear());
+
 console.log(typeof myDate);
 
-let myCreatedDate = new Date(2024,11,8)
-console.log(myCreatedDate.toDateString());
+let myCreatedDate = new Date(2024,11,10,16,25)
+console.log(myCreatedDate.toLocaleString());
+
+let myCreatedDate2 = new Date("12-10-2024")
+console.log(myCreatedDate2.toLocaleString());
+
+let myTimeStamp  = Date.now()
+console.log(myTimeStamp);
+console.log(Math.floor(myTimeStamp/1000));
+
+//mdn documentation
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date
+
+
+
+
 
 
