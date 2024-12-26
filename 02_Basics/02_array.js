24a1f8f42c0c25c03d171944e6960ef117e1ac29
lonecodebreaker <75729507+lonecodebreaker@users.noreply.github.com>
2024-12-26 13:34:47 +0000

    Arrray_completed

---
+++ 02_Basics/02_array.js
@@ -0,0 +1,38 @@
+const marvel = ["Ironman","Hulk","doctorStrange"]
+const dc  = ["superman","flash","batman"]
+
+//Approach_1
+//marvel.push(dc)
+//console.log(marvel);
+
+//Approach_2
+//const myarr = marvel.concat(dc);
+//console.log(myarr);
+
+//Approach_3(most preffered)
+//const allhero = [...marvel,...dc]
+//console.log(allhero);
+
+//const arr = [1,2,3,[4,5,6],7,[5,6,7,[1,2]]]
+
+//const arr1 = arr.flat(Infinity)
+//console.log(arr1);
+
+
+//Intresting thing for interview
+//console.log(Array.isArray("shubham"));
+//console.log(Array.from("shubham"))
+
+//console.log(Array.from({name:"shubham"}))
+
+let score1 = 100
+let score2 = 200
+let score3  = 300
+
+//console.log(Array.of(score1,score2,score3))
+
+
+
+
+
+
