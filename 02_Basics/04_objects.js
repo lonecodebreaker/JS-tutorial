164535d15c57304a326a27d7d217ff8bd21cf560
lonecodebreaker <75729507+lonecodebreaker@users.noreply.github.com>
2025-05-22 13:49:07 +0000

    object and json Api info

--- 02_Basics/04_objects.js
+++ 02_Basics/04_objects.js
@@ -23,7 +23,54 @@ const obj1 = {1: "a", 2:"b"}
 const obj2 = {3: "a", 4:"b"}
 
 //const obj3 = { obj1 , obj2 }
-const obj3 = Object.assign({},obj1 , obj2 )
-console.log(obj1);
-console.log(obj3);
+//const obj3 = Object.assign({},obj1 , obj2 )
+const obj3 = {...obj1,...obj2}
+// console.log(obj1);
+// console.log(obj3);
 
+const users = [
+    {
+        id: "1" ,
+        email: "s@gmail.com" 
+    },
+    {
+         id: "2" ,
+        email: "r@gmail.com" 
+    }
+]
+
+// console.log(users[1].email);
+
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
+
+//console.log(tinderUser.hasOwnProperty('isLogged'));
+
+
+// object destructuring and JSON api
+
+const course = {
+    courseName : "js learning",
+    price: "999",
+    courseInstructor: "myself"
+}
+
+//course.courseInstructor
+
+const {courseInstructor : masterji } = course
+console.log(masterji);
+
+//JSON API data when fetched
+// {
+//     "courseName" : "js tutorial",
+//     "price": "free",
+//     "courseInstructor":"Shubham"
+
+// }
+
+// [
+//     {},
+//     {},
+//     {}
+// ]
