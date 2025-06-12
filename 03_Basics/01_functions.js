3aba394491a181cf0789dd6cb90a721b9661cb40
lonecodebreaker <75729507+lonecodebreaker@users.noreply.github.com>
2025-06-12 17:17:19 +0000

    js learning commit

---
+++ 03_Basics/01_functions.js
@@ -0,0 +1,14 @@
+
+
+function myName(){
+     console.log("S")
+     console.log("H")
+     console.log("U")
+      console.log("B")
+      console.log("H")
+     console.log("A")
+      console.log("M")
+
+}
+
+ myName()
