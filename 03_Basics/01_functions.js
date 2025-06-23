1e5a1b09d7d6fe5b7e6f885c0f22c045c66ee0ba
lonecodebreaker <75729507+lonecodebreaker@users.noreply.github.com>
2025-06-23 18:48:58 +0530

    functions topic in depth

--- 03_Basics/01_functions.js
+++ 03_Basics/01_functions.js
@@ -39,3 +39,41 @@ function loginUserMessage(username = "sam") {
 }
 console.log(loginUserMessage());
 
+
+
+// rest operator application
+// practical application problem in shopping cart and solution
+function calculateCartPrice(val1 , val2 , ...num1){
+    return num1
+}
+console.log(calculateCartPrice(200 , 400, 500,2000));
+
+const user = {
+    username:"Shubham",
+    aged : 18
+}
+
+//object passed in function
+function handleObject(anyObject){
+    console.log(`username is ${anyObject.username} and age is ${anyObject.age}`);
+}
+
+handleObject(user)
+// object can be passed directly too
+handleObject({
+    username : "sam",
+    age: 19
+})
+
+
+//array passed in function
+const myNewArray = [ 200, 400, 100, 600 ]
+
+function returnSecondvalue(getArray){
+     return getArray[1]
+}
+
+console.log(returnSecondvalue(myNewArray));
+// array can be passed directly too
+console.log(returnSecondvalue([200, 400, 100, 600]));
+
