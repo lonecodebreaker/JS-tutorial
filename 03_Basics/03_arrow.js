25dcd6d122bd4e0f02d81ec79a458dc39df756bd
lonecodebreaker <75729507+lonecodebreaker@users.noreply.github.com>
2025-06-24 23:28:30 +0530

    arrow function learned

--- 03_Basics/03_arrow.js
+++ 03_Basics/03_arrow.js
@@ -52,7 +52,7 @@ const addTwo = (num1 , num2 ) => {
       return num1 + num2 
 }
 
-// implicit return arrow function
+// implicity returned arrow function
 const addTwoAgain = ( num1 , num2) => ( num1 + num2 )  
 console.log(addTwo(3,5));
 
