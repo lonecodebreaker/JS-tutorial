cdb4043afd3ac5395410a6b8853ae07ac5a71962
lonecodebreaker <75729507+lonecodebreaker@users.noreply.github.com>
2025-06-24 17:54:48 +0530

    hoisting and arrow function

--- 03_Basics/02_scopes.js
+++ 03_Basics/02_scopes.js
@@ -11,18 +11,59 @@ if (true) {
 
 //console.log(a);  // error as a is not defined 
 //console.log(b);  // arror as b is not defined
-console.log(c);    // output is 30
+//console.log(c);    // output is 30
 
 let a = 400
 if (true) {
     let a = 10   // value of a inside this scope is 10
-    console.log("Inner :", a);
+    //console.log("Inner :", a);
     const b = 20
     var c = 30
 }
 
-console.log(a); // output is 400 as value of a outside if scope is 400
+//console.log(a); // output is 400 as value of a outside if scope is 400
 //console.log(b);
 //console.log(c);
 
 // the global scope when using inspect element then console in browser is diffrent from when we see global scope in coding enviroment with help of node.
+
+
+//Nested scope
+
+function one() {
+    const username = "shubham"
+
+    function two() {
+        const website = "youtube"
+        //console.log(username);
+    }
+    //console.log(website);
+    two()
+
+}
+one()
+
+if (true) {
+    const username = "shubham"
+    if (username === "shubham") {
+        const website = "youtube"
+        //console.log(username + website);
+    }
+    // console.log(website); --> 1st error 
+}
+// console.log(username);  --> 2nd error
+
+
+// +++++++++++++++++++++++++++++++++ Intresting +++++++++++++++++++++++++++
+
+function addOne(num) {
+    return num + 1;
+}
+addOne(5)
+
+const addTwo = function (num) {
+    return num + 2
+}
+addTwo(5)
+
+// both addOne and addTwo are function but variable holding function is sometimes called expression. 
---
+++ 03_Basics/03_arrow.js
@@ -0,0 +1,63 @@
+
+//this keyword inside object
+const user = {
+    username : "shubham",
+    price: 999 ,
+
+    welcomeMessage : function(){
+        console.log(`${this.username}, welcome to website`);
+        console.log(this);
+        
+    }
+}
+user.welcomeMessage()
+user.username = "sam"
+user.welcomeMessage()
+console.log(this);
+
+
+
+//this keyword inside functions 
+function coffee(){
+    let username = "shubham"
+    console.log(this.username);
+}
+coffee()
+
+
+//arrow function - how its made 
+
+const minimalist = function(){
+    let username = "Shubham"
+    console.log(this.username);
+    console.log(this);
+    
+}
+minimalist()
+ 
+// converted to arrow function like this 
+
+const minimalist2 = () => {
+    let username = "Shubham"
+    console.log(this.username);
+    console.log(this);
+    
+}
+minimalist()
+
+
+// working on pure arrow function 
+
+const addTwo = (num1 , num2 ) => {
+      return num1 + num2 
+}
+
+// implicit return arrow function
+const addTwo = ( num1 , num2) => ( num1 + num2 )  
+console.log(addTwo(3,5));
+
+// implicitly returning object
+const addObject = (num1 , num2 ) => ({username : "shubham"})
+console.log(addObject(3,5));
+
+
