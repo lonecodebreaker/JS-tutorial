5dd65fb2b1afbb767a68f0565dc34801ce2e9ebc
lonecodebreaker <75729507+lonecodebreaker@users.noreply.github.com>
2025-06-28 21:52:38 +0530

    switch_case explained

--- 04_Control_Flow/switch.js
+++ 04_Control_Flow/switch.js
@@ -1,8 +1,34 @@
-switch (key) {
-    case value:
-        
-        break;
 
-    default:
+// // Syntax od switch case
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+// Here key is the variable thats value want to check everytime 
+
+
+const month = 3
+
+switch(month){
+    case 1: 
+        console.log("january");
         break;
+    case 2: 
+        console.log("february");
+        break;    
+    case 3: 
+        console.log("march");
+        break;    
+    case 4: 
+        console.log("april");
+        break;       
+    default:
+        console.log("deafult case match");
+        break;    
+
 }
