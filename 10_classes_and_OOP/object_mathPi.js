80753fbb933ee02f783754e67ef747094e8efa03
lonecodebreaker <75729507+lonecodebreaker@users.noreply.github.com>
2025-07-30 08:51:30 +0530

    object deep knowldege

--- 10_classes_and_OOP/object_mathPi.js
+++ 10_classes_and_OOP/object_mathPi.js
@@ -1,13 +1,51 @@
 // Here we will know deeply the object in js , like how js handle object internally
 
-// Object.getOwnPropertyDescriptor()
+Object.getOwnPropertyDescriptor()
 
-// console.log(Math.PI);
-// Math.PI = 5;
+console.log(Math.PI);
+Math.PI = 5;
 
-// //after assigning something diffrent from original value
-// console.log(Math.PI);
+//after assigning something diffrent from original value
+console.log(Math.PI);
 
 const descripter = Object.getOwnPropertyDescriptor(Math,"PI")
 console.log(descripter);
+
+
+// Object created through factory function
+const myNewObj = Object.create(null)
+
+// Object Created directly
+const User = {
+   username : "Shubham",
+   age : 26,
+   isLoggedIn : true,
+   work : function(){
+    console.log("user is working ");
+   }
+}
+
+console.log(User);
+// getting the use of property descriptor in user defined property 
+
+console.log(Object.getOwnPropertyDescriptor(User,"username"));
+
+//looping in Object
+for (let [key,value] of Object.entries(User)) {
+    if(typeof value !== 'function'){
+        console.log(`${key} : ${value}`);
+    }
+}
+
+// stooping looping in a property of Object
+Object.defineProperty(User , "username" , {
+    writable : false ,
+    enumerable : false
+})
+
+console.log(Object.getOwnPropertyDescriptor(User,"username"));
+
+// Now if object properties got looped , username will not be there .
+
+
 
